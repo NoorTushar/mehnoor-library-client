cf814a86e3048e83a84a664cbc92f6c0ac16b2ea
NoorTushar <tushar.noor.khan@gmail.com>
2025-07-06 20:51:38 +0600

    setup redux and base api

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+   reducerPath: "baseApi",
+   baseQuery: fetchBaseQuery({
+      baseUrl: "https://mehnoorlibrary.vercel.app/api",
+   }),
+   tagTypes: ["book"],
+   endpoints: (builder) => ({
+      getTasks: builder.query({
+         query: () => "/books",
+         providesTags: ["book"],
+      }),
+      //   createTask: builder.mutation({
+      //      query: (taskData) => ({
+      //         url: "/tasks",
+      //         method: "POST",
+      //         body: taskData,
+      //      }),
+      //      invalidatesTags: ["task"],
+      //   }),
+      //   deleteTask: builder.mutation({
+      //      query: (taskId) => ({
+      //         url: `/tasks/${taskId}`,
+      //         method: "DELETE",
+      //      }),
+      //      invalidatesTags: ["task"],
+      //   }),
+   }),
+});
+
+// export const {
+//    useGetTasksQuery,
+//    useCreateTaskMutation,
+//    useDeleteTaskMutation,
+// } = baseApi;
